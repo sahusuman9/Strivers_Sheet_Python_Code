33a285ef016c680531af37ffa87a0800c21cf1be
Suman Kumar Sahu <suman.sahu.981@gmail.com>
2026-03-24 16:34:06 +0530

    first commit

---
+++ DSA_01.py
@@ -0,0 +1,14 @@
+"""
+Q1. Write a python program to find the largest element in an array.
+"""
+from typing import List
+def find_largest_element(n: int, arr: List[int]) -> int:
+    max = arr[0]
+    for i in range(1, n):
+        if arr[i] > max:
+            max = arr[i]
+    return max
+
+
+array = list(map(int, input("Enter the elements of the array: ").split()))
+print("The largest element in the array is: ", find_largest_element(len(array), array))
